2c54259c8e40b2b38f5f156f6b05957da5be5adc
varun <varun24@192.168.1.23>
2024-03-19 12:33:11 +0530

    removed console logs

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Link from "next/link";
 
 const Form = ({ type, submitting, post, setPost, handleSubmit }) => {
-  console.log(post);
   return (
     <section className="w-full max-w-full  flex-start flex-col ">
       <h1 className="head_text text-left">
