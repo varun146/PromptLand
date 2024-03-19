595796651753c474e53ddfef0e355070ecad2758
varun <varun24@192.168.1.23>
2024-03-19 12:27:10 +0530

    implemented search

--- app/components/Feed.jsx
+++ app/components/Feed.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
@@ -19,31 +19,42 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  const handleSearchChange = async (e) => {
-    setSearchText(e.target.value);
-  };
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const searchInput = useRef();
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch("/api/prompt");
       const data = await res.json();
       setPosts(data);
+      setFilteredPosts(data);
     };
     fetchPosts();
   }, []);
+
+  // re-renders when the seachTest changes
+  useEffect(() => {
+    const filteredData = posts.filter(
+      (post) =>
+        post.prompt.toLowerCase().includes(searchText) &&
+        post.creator.username.toLowerCase().includes(searchText),
+    );
+    setFilteredPosts(filteredData);
+  }, [searchText, posts]);
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
+          ref={searchInput}
           type="text"
           placeholder="Search for a tag or a username"
           value={searchText}
-          onChange={handleSearchChange}
+          onChange={(e) => setSearchText(e.target.value)}
           required
           className="w-full bg-gray-300 text-black text-sm py-4 px-3 rounded-full focus:outline-none shadow-md shadow-gray-400"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList data={filteredPosts} handleTagClick={() => {}} />
     </section>
   );
 };
